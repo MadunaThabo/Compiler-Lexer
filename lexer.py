af4ff0e2f16197577cf2ada256e121126a46cd4f
Thabo Maduna <madunathabo2@gmail.com>
2022-04-15 13:59:05 +0200

    Add files via upload

---
+++ lexer.py
@@ -0,0 +1,11 @@
+import operator
+
+
+file = open("code.txt", 'a+')
+
+identifiers = {"main":"main identifier", }
+dataType = {"num":"number", "string":"text string", "bool":"boolean"}
+binaryOperators = {"and":"and operator", "or":"or operator", "eq":"equal operator", "larger":"larger operator", "add":"addition operator", "sub":"subtraction operator", "mult":"multiplication operator"}
+unaryOperator = {}
+
+
