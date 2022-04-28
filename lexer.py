1bcf89b5271a0544aa8c0f9d5cc5e4d75140ba30
OomBen <u19116498@tuks.co.za>
2022-04-28 23:49:18 +0200

    🐛 (lexer): fixed short string lexer

--- lexer.py
+++ lexer.py
@@ -8,6 +8,7 @@ dataType = {
     "num":"number",
     "string":"text string",
     "bool":"boolean"}
+
 binaryOperators = {
     "and":"and operator",
     "or":"or operator",
@@ -16,13 +17,16 @@ binaryOperators = {
     "add":"addition operator",
     "sub":"subtraction operator",
     "mult":"multiplication operator"}
+
 unaryOperators = {
     "input":"input operation",
     "not":"not operation",
    }
+
 constants = {
     "true":"true boolean",
     "false":"false boolean"}
+
 keywords = {
     "main":"main keyword",
     "if":"if keyword",
@@ -46,10 +50,14 @@ symbols = {
     "(":"open bracket",
     ")":"close bracket",
     ",":"comma",
+    }
+
+assign = {
     ":=":"assign operator"
     }
 
 def checkIfNumber(i):
+    # TODO: check if only a dash
     if(len(i) > 1 and i[0]=="0"):
         output = "Number read is: "  + i + "returns LEXICAL ERROR"+"\n"
         file2.write(output)
@@ -79,7 +87,6 @@ def checkIfNumber(i):
         output = "Symbol read is: "  + i + " - positive number\n"
         file2.write(output)
 
-
 def checkIfUserDefined(i):
     letters =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     numbers = ["0","1","2","3","4","5","6","7","8","9"]
@@ -100,31 +107,29 @@ def checkIfUserDefined(i):
         output = "Symbol read is: "  + i + " - User Defined name\n"
         file2.write(output)
 
-
-def checkIfShortString(i):
+def checkIfShortString(i, stringLen):
     shortStrings = [""," ","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     numbers = ["0","1","2","3","4","5","6","7","8","9"]
-    if len(i)>=16:
+    if stringLen>=16:
         output = "short string is too long"
         file2.write(output)
         return
 
-    if(i[0] != "\"" or i[len(i)-1] != "\""):
+    elif(i[0] != "\"" or i[len(i)-1] != "\""):
         output = "string read is: "  + i + " returns LEXICAL ERROR- make sure has opening and closing \""+"\n"
         file2.write(output)
     else:
         for k in range(1, len(i)-1):
-            if(i[k] not in shortStrings or i[k] not in numbers or i[k].isspace() == False ):
-                output = "string read is: "  + i + " returns LEXICAL ERROR-  only capital letters are allowed and no special characters allowed"+"\n"
+            if(i[k] in shortStrings or i[k] in numbers or i[k].isspace()):
+                continue
+            else:
+                output = "string read is: "  + i + " returns LEXICAL ERROR-  only capital letters and numbers are allowed and no special characters allowed"+"\n"
                 file2.write(output)
                 return
-            else:
-                continue
-        output = "string read is: " + i + " - short string"
+        output = "string read is: " + i + " - short string\n"
+        file2.write(output)
                 
-
 def doCheck(i):
-    print("in do check: ", i,"is being checked")
     if(len(i)<=0):
         return
     if i in dataType:
@@ -147,14 +152,13 @@ def doCheck(i):
         file2.write(output)
     elif i[0].isdigit() or i[0]=="-":
         checkIfNumber(i)
-    elif i[0] == "\"":
-        checkIfShortString(i)
+    # elif i[0] == "\"":
+    #     checkIfShortString(i)
     elif i[0] in letters:
         checkIfUserDefined(i)
     else:
         output = "symbol read is: " + i + " is undefined\n"
         file2.write(output)
-
 
 output =""
 file2 = open("output.txt", "a+")
@@ -163,69 +167,52 @@ topology = file.readlines()
 for line in topology:
     line = line.rstrip()
     word = ""
+    last=-1
     for i in range(0,len(line)):
-        # print("word is: ",word)
-        # if line[i] == "\"":
-        #     l = 0
-        #     found = False
-        #     for k in range(i,len(line)):
-        #         if line[k] != "\"":
-        #             l+=1
-        #             word+=line[k]
-        #         else:
-        #             word+=line[k]
-        #             found = True
-        #             break
-        #     if found:
-        #         checkIfShortString(word,l)        
-        #     i = k
-        if(line[i].isspace() or line[i] in symbols):
+        if i<= last:
+            continue
+        elif line[i] == ":":
+            if line[i+1] == "=":
+                output = "symbol read is: := - assign operation\n"
+                file2.write(output)
+                last = i+2
+                word=""
+        elif line[i] == "\"":
+            print("checking for short string", word)
+            l = 0
+            found = False
+            last=0
+            for k in range(i,len(line)):
+                print(line[k]+"this is our character", found)
+                if line[k] != "\"":
+                    l+=1
+                    word+=line[k]
+                    last = k
+                else:
+                    word+=line[k]
+                    found = True
+                    last = k
+                    if(k >i):
+                        print("Checking why we here ", k)
+                        break
+                   
+            if found:
+                checkIfShortString(word,l) 
+            else:
+                output = "No closing \" for "+word
+                file2.write(output)       
+            i = last
+            word=""
+        elif(line[i].isspace() or line[i] in symbols):
             if line[i].isspace():
                 doCheck(word)
                 word = ""
-                print("got space")
             elif line[i] in symbols:
                 doCheck(word)
                 doCheck(line[i])
                 word = ""
         else: 
             word+=line[i]
-
-        
-        
-        # if line[i].isspace() or line[start:3] in symbols or line[i] in symbols or line[i] in numbers:
-        #     if line[i].isspace():
-        #         print("start assigned at space", i, line[i])
-        #         output = "space read\n"
-        #         file2.write(output)
-        #         start = i+1
-
-        #     elif line[i]==":":
-        #         if i+1<len(line) and line[i+1]=="=":
-        #             output = "assign operator: "+ line[start:i+1] + "\n"
-        #             print("assign Start is: ", i," : ", line[start+1])
-        #             file2.write()
-        #         else:
-        #             output = "lexor error: "+ line[i] + "\n"
-        #             file2.write(output)
-        #     elif line[i] in symbols:
-        #         output = "Symbol: "+ line[i] + "\n"
-        #         start = i
-        #         print("symbol Start is: ", i," : ", line[start])
-        #         file2.write(output)
-
-        #     elif line[i] in numbers:
-        #         output = "number: "+ line[i] + "\n"
-        #         start = i+1
-        #         print("number Start is: ", i," : ", line[start])
-        #         file2.write(output)
-
-        #     elif line[start:i] in unaryOperators:
-        #         print("checking word: ", line[start:i])
-        #         output = "Unary operator: "+ line[start:i] + "\n"
-        #         start = i
-        #         print("unary Start is: ", i," : ", line[start])
-        #         file2.write(output)
 
             
  
